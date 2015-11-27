52518b29b97c43557d07687121ae6182a08f98a2
Diethard Jansen <diethard.jansen@gmail.com>
2015-11-27 09:23:47 +0100

    include statements to recalculate extents

--- sql/clip_zwartsluis.sql
+++ sql/clip_zwartsluis.sql
@@ -21,3 +21,29 @@ WHERE Disjoint(geometry, (select geometry from woonplaats where id='2201'))
 --
 DELETE from standplaats
 WHERE Disjoint(geometry, (select geometry from woonplaats where id='2201'))
+--
+select InvalidateLayerStatistics('ligplaats')
+--
+select InvalidateLayerStatistics('standplaats')
+--
+select InvalidateLayerStatistics('pand')
+--
+select InvalidateLayerStatistics('verblijfsobject')
+--
+select InvalidateLayerStatistics('verblijfsobject', 'geometry')
+--
+select InvalidateLayerStatistics('ligplaats', 'geometry')
+--
+select InvalidateLayerStatistics('standplaats', 'geometry')
+--
+select InvalidateLayerStatistics('pand', 'geometry')
+--
+select UpdateLayerStatistics('ligplaats')
+--
+select UpdateLayerStatistics('standplaats')
+--
+select UpdateLayerStatistics('pand')
+--
+select UpdateLayerStatistics('verblijfsobject')
+--
+vacuum
